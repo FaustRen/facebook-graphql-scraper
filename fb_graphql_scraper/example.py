abcc8acdf054f90e63d12d49ea17337413e2220d
ShangRenHong <faustren1z@gmail.com>
2024-05-08 11:07:36 +0800

    docs(remove example ipynb ):

--- fb_graphql_scraper/example.py
+++ fb_graphql_scraper/example.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-from fb_graphql_scraper.facebook_graphql_scraper import FacebookGraphqlScraper as fb_graphql_scraper
+# from fb_graphql_scraper.facebook_graphql_scraper import FacebookGraphqlScraper as fb_graphql_scraper
 
 
 ## Example.1 - without logging in
-if __name__ == "__main__":
-    facebook_user_name = "love.yuweishao"
-    facebook_user_id = "100044253168423"
-    days_limit = 30 # Number of days within which to scrape posts
-    driver_path = "/Users/renren/Desktop/FB_graphql_scraper拷貝/fb_graphql_scraper/resources/chromedriver-mac-arm64/chromedriver" 
-    fb_spider = fb_graphql_scraper(driver_path=driver_path)
-    res = fb_spider.get_user_posts(fb_username_or_userid=facebook_user_name, days_limit=days_limit,display_progress=True)
-    print(res)
+# if __name__ == "__main__":
+#     facebook_user_name = "love.yuweishao"
+#     facebook_user_id = "100044253168423"
+#     days_limit = 30 # Number of days within which to scrape posts
+#     driver_path = "/Users/renren/Desktop/FB_graphql_scraper拷貝/fb_graphql_scraper/resources/chromedriver-mac-arm64/chromedriver" 
+#     fb_spider = fb_graphql_scraper(driver_path=driver_path)
+#     res = fb_spider.get_user_posts(fb_username_or_userid=facebook_user_name, days_limit=days_limit,display_progress=True)
+#     print(res)
 
 
 ## Example.2 - login in your facebook account to collect data
