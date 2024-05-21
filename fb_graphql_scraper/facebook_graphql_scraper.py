107be54a0bfcde8781cdddd0ca793165f84c160b
ShangRenHong <faustren1z@gmail.com>
2024-05-21 23:22:05 +0800

    fix(language, profile info, checking progress):
    1. The program will execute the crawler on English pages
    2. Fixed the problem of being unable to obtain profile
    3. Fixed an issue with the check progress mechanism

--- fb_graphql_scraper/facebook_graphql_scraper.py
+++ fb_graphql_scraper/facebook_graphql_scraper.py
@@ -10,6 +10,7 @@ from fb_graphql_scraper.utils.parser import RequestsParser
 from fb_graphql_scraper.utils.locator import *
 from fb_graphql_scraper.utils.utils import *
 
+
 class FacebookSettings:
     """ How to use:
     from fb_graphql_scraper.facebook_graphql_scraper import FacebookGraphqlScraper as fb_graphql_scraper
@@ -106,22 +107,25 @@ class FacebookGraphqlScraper(FacebookSettings):
             tmp_creation_array=tmp_creation_array)
         if self.pre_diff_days == diff_days:
             self.counts_of_same_diff_days += 1
+        else:
+            self.counts_of_same_diff_days = 0
         self.pre_diff_days = max(diff_days, self.pre_diff_days)
         if display_progress:
             print(f"To access posts acquired within the past {self.pre_diff_days} days.") # 已取得n日內貼文
         return is_date_exceed_limit(max_days_ago=diff_days, days_limit=days_limit)
     
-    def get_profile_feed(self):
+    def get_profile_feed(self, dict_in:dict={"data-pagelet": "ProfileTilesFeed_0"}):
+        time.sleep(2)
         page_source = (self.page_optional.driver.page_source)
         soup = BeautifulSoup(page_source, "html.parser")
-        target_div = soup.find("div", {"data-pagelet": "ProfileTilesFeed_0"})
+        target_div = soup.find("div", dict_in)
         if target_div:
             texts = target_div.find_all(text=True)
         return texts[2::]
     
     def get_plugin_page_followers(self, fb_username_or_userid):
         """透過嵌入式貼文取得粉絲專頁追蹤人數"""
-        plugin_page_url = f"https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F{fb_username_or_userid}&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        plugin_page_url = f"https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F{fb_username_or_userid}&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId&locale=en_us"
         plugin_response = requests.get(url=plugin_page_url)
         plugin_soup = BeautifulSoup(plugin_response.text, "html.parser")
         return plugin_soup.find("div", class_="_1drq").text
@@ -163,7 +167,7 @@ class FacebookGraphqlScraper(FacebookSettings):
         return reactions_out
 
     def get_user_posts(self, fb_username_or_userid: str, days_limit: int = 61, display_progress:bool=True) -> dict:
-        url = "https://www.facebook.com/"+fb_username_or_userid # 建立完整user連結
+        url = f"https://www.facebook.com/{fb_username_or_userid}?locale=en_us" # 建立完整user連結
         self.page_optional.load_next_page(url=url, clear_limit=20)# driver 跳至該連結
         self.page_optional.load_next_page(url=url, clear_limit=20)# 徹底清除requests避免參雜上一用戶資料
         self.requests_parser._clean_res() # 清空所有用於儲存結果的array
@@ -172,11 +176,14 @@ class FacebookGraphqlScraper(FacebookSettings):
 
         # If you did not login, click X button
         if self.fb_account == None: self.page_optional.click_reject_login_button()
-        
+
         # Get profile information
         try:
-            profile_feed = self.get_profile_feed()
-            if "粉絲專頁" in profile_feed: # 如果粉絲專頁在profile_feed, 另外request plugins page來取得追蹤人數
+            if self.fb_account != None:
+                profile_feed = self.get_profile_feed(dict_in={"class": "x1yztbdb"})
+            else:
+                profile_feed = self.get_profile_feed()
+            if "Page" in profile_feed or "page" in profile_feed:
                 followers = self.get_plugin_page_followers(fb_username_or_userid=fb_username_or_userid)
                 profile_feed.append(followers)
         except Exception as e:
@@ -185,22 +192,25 @@ class FacebookGraphqlScraper(FacebookSettings):
 
         # Scroll page
         counts_of_round = 0
-        for _ in range(1000): # 滾動最大次數
+        for _ in range(1000): # max rounds of scrolling page
             self.page_optional.scroll_window()
-            if counts_of_round >= 10:  # 每滾10次檢查一次, 
+            if counts_of_round >= 5:  # Check progress every 5 times you scroll the page
                 if display_progress:
                     print("Check spider progress..")
                 if self.check_progress(days_limit=days_limit,display_progress=display_progress):
                     break
-                elif self.counts_of_same_diff_days >= 3:  # 如果找到的最遠日連續三次都是同一天表示頁面可能滾到底了
+                # If you find that the published dates 
+                # of the collected posts are on the same day five times in a row, 
+                # it may mean that the page has scrolled to the bottom.
+                elif self.counts_of_same_diff_days >= 5:
                     break
                 else:
                     counts_of_round = 0
 
             counts_of_round += 1
-            pause(0.3)
+            pause(0.7)
 
-        # Collect data, 利用driver請求列表當中的GraphQL來取得資料
+        # Collect data, extract graphql from driver requests.
         driver_requests = self.page_optional.driver.requests
         for req in driver_requests:
             req_response, req_url = req.response, req.url
